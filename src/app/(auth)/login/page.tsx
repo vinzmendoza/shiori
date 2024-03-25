c6f8bcf512e9612ce1e6123104f3be38e681faa4
vinzmendoza <vinzkarloemendoza@gmail.com>
2024-03-25 17:57:21 +0800

    add login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ export default async function LoginPage() {
     const { data, error } = await supabase.auth.getUser();
 
     if (data?.user) {
-        redirect('/')
+        redirect('/category/general')
     }
 
     return (
